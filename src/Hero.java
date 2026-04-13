da0c07194a9f2d56fc4df9426e3378b03d0ceb9b
kaimatter <kaifmatter@gmail.com>
2026-04-13 11:25:48 -0600

    Added Hero class

---
+++ src/Hero.java
@@ -0,0 +1,22 @@
+public class Hero {
+    private int hitPoints;
+    private int damage;
+
+
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+}
